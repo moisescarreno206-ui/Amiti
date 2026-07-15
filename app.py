003aef1c23594f4591cace06e01a13fa076fc066
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-07-15 11:47:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template, request, jsonify
-# La importación debe ser relativa a la carpeta nucleos
 from nucleos.n00_nucleo_maestro import amiti
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    # Asegúrate de que index.html esté dentro de la carpeta 'templates'
+def index():
     return render_template('index.html')
 
 @app.route('/procesar', methods=['POST'])
 def procesar():
     data = request.json
-    comando = data.get('comando', '')
-    return jsonify({"respuesta": amiti.procesar(comando)})
+    res = amiti.procesar(data.get('comando', ''))
+    return jsonify({"respuesta": res})
 
 if __name__ == '__main__':
     app.run()
