bb1f999338c9bcb9fa4e4f5477b672cc2b3893a4
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-07-15 12:31:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, jsonify
-# La ruta correcta es nucleos.n00_nucleo_maestro
 from nucleos.n00_nucleo_maestro import amiti
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
-# ... resto de tu código ...
+
+@app.route('/procesar', methods=['POST'])
+def ejecutar():
+    data = request.json
+    res = amiti.procesar(data.get('comando', ''))
+    return jsonify({"respuesta": res})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
